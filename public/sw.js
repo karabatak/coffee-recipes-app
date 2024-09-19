221b1c4f545d5cceb28e7619cd6f7225636d7f54
Oktay Karabatak <oktay.karabatak@gmail.com>
2024-09-19 20:01:13 +0300

    dynamic cache added

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/icons/icon-512x512.png',
 ];
 
 // Service worker kurulumunda önbelleğe alma
@@ -16,12 +16,29 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Service worker ile fetch olayını yönetme
+// Service worker ile fetch olayını yönetme (Dinamik Önbellekleme)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Eğer önbellekte varsa, önbellekten döner, yoksa ağdan ister
-      return response || fetch(event.request);
+      if (response) {
+        return response;
+      }
+
+      // Dinamik olarak önbelleğe alma
+      return fetch(event.request).then((networkResponse) => {
+        // Eğer istek başarısız olursa (çevrimdışı vb.), boş bir yanıt döndür
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+
+        // Önbelleğe ekleme
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return networkResponse;
+      });
     })
   );
 });
