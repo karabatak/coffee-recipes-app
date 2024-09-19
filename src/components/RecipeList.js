523d0d459cf5ade91a7780d63e5fd3bc424fdc2b
Oktay Karabatak <oktay.karabatak@gmail.com>
2024-09-19 19:28:02 +0300

    card and font size update

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,13 +3,13 @@ import recipes from '../data/recipes';
 
 function RecipeList() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="w-full grid grid-cols-1 gap-4 md:grid-cols-1 lg:grid-cols-3">
       {recipes.map((recipe) => (
-        <div key={recipe.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-          <img src={recipe.image} alt={recipe.name} className="w-full h-48 object-cover" />
+        <div key={recipe.id} className="w-full bg-white rounded-lg shadow-md overflow-hidden">
+          <img src={recipe.image} alt={recipe.name} className="w-full h-85 object-cover" />
           <div className="p-4">
-            <h2 className="text-xl font-semibold mb-2">{recipe.name}</h2>
-            <p className="text-gray-700">{recipe.description}</p>
+            <h2 className="text-3xl font-semibold mb-2">{recipe.name}</h2>
+            <p className="text-xl text-gray-700">{recipe.description}</p>
           </div>
         </div>
       ))}
