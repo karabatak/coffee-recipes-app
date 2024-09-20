9ffcaa9dfdb4d8960edd346afbd5c5609a2b647c
Oktay Karabatak <oktay.karabatak@gmail.com>
2024-09-20 10:55:50 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coffee-recipes-cache-v43'; // Her yeni versiyon çıktığında bu değeri değiştirin
+const CACHE_NAME = 'coffee-recipes-cache-v44'; // Her yeni versiyon çıktığında bu değeri değiştirin
 const urlsToCache = [
   '/coffee-recipes-app/',
   '/coffee-recipes-app/manifest.json',
